919ca7f34c774751a9b1b240d54e3db63a67d67f
AurelioMiranda <80250266+AurelioMiranda@users.noreply.github.com>
2024-10-17 23:17:35 +0100

    Updated pricing for new items.
    Updated the pricing section for temporary tattoos, vinyl stickers and glass stickers.

--- src/app/precos/page.js
+++ src/app/precos/page.js
@@ -6,24 +6,54 @@ export default function Precos() {
       <section className={styles.precosSection987}>
         <h1 className={styles.precosTitle987}>Preçário</h1>
 
+        <h2 className={styles.precosTitle958}>Stickers Clássicos</h2>
+
         <div className={styles.priceImageContainer987}>
           <img src="square-or-rectangle-price.jpeg" alt="Stickers price per unit (rectangular)" className={styles.precoImage987} />
-          <p className={styles.imageCaption987}>Figura 1- Preço por unidade dos stickers quadrangulares e retangulares.</p>
+          <p className={styles.imageCaption987}>Figura 1 - Preço por unidade dos stickers quadrangulares e retangulares.</p>
         </div>
 
         <div className={styles.priceImageContainer987}>
           <img src="circular-or-customized-price.jpeg" alt="Stickers price per unit (circular/customizable)" className={styles.precoImage987} />
-          <p className={styles.imageCaption987}>Figura 2- Preço por unidade dos stickers com corte circular/customizado</p>
+          <p className={styles.imageCaption987}>Figura 2 - Preço por unidade dos stickers com corte circular/customizado.</p>
+        </div>
+
+        <h2 className={styles.precosTitle958}>Stickers Premium de Vinyl</h2>
+
+        <div className={styles.priceImageContainer987}>
+          <img src="vinyl-square-rectangular.jpeg" alt="Stickers price per unit (rectangular vinyl)" className={styles.precoImage987} />
+          <p className={styles.imageCaption987}>Figura 3 - Preço por unidade dos stickers de vinyl quadrangulares e retangulares.</p>
         </div>
 
         <div className={styles.priceImageContainer987}>
+          <img src="vinyl-custom-circular.jpeg" alt="Stickers price per unit (circular/customizable vinyl)" className={styles.precoImage987} />
+          <p className={styles.imageCaption987}>Figura 4 - Preço por unidade dos stickers de vinyl com corte circular/customizado.</p>
+        </div>
+
+        <h2 className={styles.precosTitle958}>Stickers para Superfícies de Vidro</h2>
+
+        <div className={styles.priceImageContainer987}>
+          <img src="glass-stickers.jpeg" alt="Stickers price per unit (glass)" className={styles.precoImage987} />
+          <p className={styles.imageCaption987}>Figura 5 - Preço por unidade dos stickers para vidro quadrangulares.</p>
+        </div>
+
+        <h2 className={styles.precosTitle958}>Tatuagens temporárias</h2>
+
+        <div className={styles.priceImageContainer987}>
+          <img src="temporary-tattoos.jpeg" alt="Temporary tattoos" className={styles.precoImage987} />
+          <p className={styles.imageCaption987}>Figura 6 - Preço por unidade das tatuagens temporárias.</p>
+        </div>
+
+        <h2 className={styles.precosTitle958}>Serviços e tarifas</h2>
+
+        <div className={styles.priceImageContainer987}>
           <img src="shipping-costs.jpeg" alt="Shipping costs" className={styles.precoImage987} />
-          <p className={styles.imageCaption987}>Figura 3- Portes de envio</p>
+          <p className={styles.imageCaption987}>Figura 7 - Portes de envio.</p>
         </div>
 
         <div className={styles.priceImageContainer987}>
           <img src="treatment-prices.jpeg" alt="Treatment prices" className={styles.precoImage987} />
-          <p className={styles.imageCaption987}>Figura 4- Preço do tratamento de imagem, em função da quantidade encomendada.</p>
+          <p className={styles.imageCaption987}>Figura 8 - Preço do tratamento de imagem, em função da quantidade encomendada.</p>
         </div>
 
         <div className={styles.deliveryInfo987}>
